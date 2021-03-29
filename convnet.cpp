1bcc8324220ab41b4f513380ff540d243c5f3de1
modakshantanu <modakshantanu@gmail.com>
2021-03-29 23:08:30 +0800

    More progress on cpp version

--- convnet.cpp
+++ convnet.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include "netio.h"
+#include "dataset.h"
 using namespace std;
 
 vector<vector<vector<float>>> filter1, filter2;
@@ -8,17 +9,24 @@ vector<vector<float>> linear;
 vector<pair<vector<vector<float>>, int>> dataset;
 
 
+void apply_filter(vector<vector<float>> &input, vector<vector<vector<float>>> &filters, int padding = 2) {
+    
+    if (input.size() != filters[0].size()) {
+        cout<<"DIMENSION ERROR";
+    }
 
-void append_to_dataset(vector<pair<vector<vector<float>>, int>> &target, string path, int label) {
-    ifstream file;
-    file.open(path);
+    vector<vector<float>> padded(input.size(), vector<float>(input[0].size() + padding*2, 0));
+    for (int i = 0; i < input.size(); i++) {
+        for (int j = 0; j < input[0].size(); j++) {
+            padded[i][j+padding] = input[i][j];
+        }
+    }
 
-    int num_samples;
-    file>>num_samples;
-
-    while(num_s)
+    
 
 }
+
+
 
 int main() {
     filter1 = vector<vector<vector<float>>>(8, vector<vector<float>>(5, vector<float>(5)));
@@ -39,6 +47,7 @@ int main() {
 
     append_to_dataset(dataset, "data/capstone/24Mar/gun_combined.csv", 1);
 
+    cout<<dataset.size();
 
 
 
---
+++ dataset.h
@@ -0,0 +1,55 @@
+#ifndef DATASET_H
+#define DATASET_H
+#include <bits/stdc++.h>
+using namespace std;
+
+void append_to_dataset(vector<pair<vector<vector<float>>, int>> &dataset, string path, int label) {
+    ifstream file;
+    file.open(path);
+
+    int num_samples;
+    file>>num_samples;
+
+    while(num_samples--) {
+        int time_samples;
+        file>>time_samples;
+
+        vector<vector<float>> raw_samples(5, vector<float>(time_samples));
+        vector<vector<float>> subset(5, vector<float>(40));
+
+
+        for (int i = 0; i < time_samples; i++) {
+            string csvline;
+            file>>csvline;
+            stringstream str(csvline);
+            // 11 12 13 16 17
+            for (int j = 0; j < 18; j++) {
+                int tmp;
+                string ts;
+                getline(str, ts, ',');
+
+                if (j != 0 && j != 9)
+                    tmp = stoi(ts);
+
+
+                if (j == 11) raw_samples[0][i] = tmp;
+                if (j == 12) raw_samples[1][i] = tmp;
+                if (j == 13) raw_samples[2][i] = tmp;
+                if (j == 16) raw_samples[3][i] = tmp;
+                if (j == 17) raw_samples[4][i] = tmp;
+            }
+
+        }
+        for (int i = 0; i < time_samples - 40 + 1; i++) {
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 40; k++) {
+                    subset[j][k] = raw_samples[j][i+k];
+                }
+            }
+            dataset.push_back({subset, label});
+        }
+
+    }
+}
+
+#endif
